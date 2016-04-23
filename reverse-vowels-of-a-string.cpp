6b4451c906623e67afa3e82e051993e56ea067b9
TauBai <traumland@hotmail.com>
2016-04-23 17:29:40 +0800

    Create reverse-vowels-of-a-string.cpp

---
+++ reverse-vowels-of-a-string.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+string reverseVowels(string s);
+
+int main()
+{
+    string s ="helll";
+    cout << reverseVowels(s);
+}
+string reverseVowels(string s) {
+        string::iterator iter = s.begin();
+        string::iterator iterend = s.end();
+        vector<string::iterator> iteratorbox;
+        for(;iter != iterend ; ++iter){
+            if((*iter) == 'a' ||
+               (*iter) == 'e' ||
+               (*iter) == 'i' ||
+               (*iter) == 'o' ||
+               (*iter) == 'u'){
+                   iteratorbox.push_back(iter);
+               }
+            
+        }
+        if(!iteratorbox.size())
+        {
+            auto itbegin = iteratorbox.begin();
+            auto itend =   iteratorbox.end() ;
+            --itend;
+            for(;itbegin < itend ; ++itbegin, --itend)
+            {            
+                char temp = **itbegin;
+              *(*itbegin) = *(*itend); 
+              *(*itend) = temp;
+            }
+        }
+        return s;
+}
