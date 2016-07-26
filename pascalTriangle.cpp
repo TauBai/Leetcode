eb780f73f70b2dadaafb782f257a2e55125ccf15
TauBai <traumland@hotmail.com>
2016-07-26 21:28:52 +0800

    Update pascalTriangle.cpp

--- pascalTriangle.cpp
+++ pascalTriangle.cpp
@@ -34,3 +34,36 @@ public:
         return result;
     }
 };
+
+/*
+
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+
+Subscribe to see which companies asked this question
+
+Show Tags
+Show Similar Problems
+
+
+*/
+// O(k) space
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> result(rowIndex+1);
+        for(int i= 0; i <= rowIndex; ++i){
+            result[i] = 1;
+            for(int t = i-1; t > 0;--t){
+                result[t] = result[t] +result[t-1];
+            }
+            
+        }
+        return result;
+    }
+};
